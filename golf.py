0648337237a01e7d2e71cf0cabc66357e10993af
ampx2c <ampx2c@mst.edu>
2020-10-08 17:55:53 -0500

    added additional comments

--- golf.py
+++ golf.py
@@ -1,5 +1,6 @@
 import random
 
+#calculates the average point value of cards remaining in the deck
 def calculateAvg(deck):
     total = 0
     for card in deck:
@@ -18,6 +19,7 @@ def calculateX(deck, hands, handsShowing): #don't go out if opponent has better
                 numCards += 1
     return total / numCards
 
+#if two cards in the same row match, add 0 to point total, otherwise add their normal point value (which is outlined in function "getValue" below)
 def calculateHand(hand):
     handTotal = 0
     if (hand[0][0] == hand[1][0] and hand[0][0] != 'X' and hand[0][0] != '-'):
@@ -37,7 +39,7 @@ def calculateHand(hand):
         handTotal += getValue(hand[5])
     return handTotal
 
-def getValue(card):
+def getValue(card): #point value for each card
     denom = card[0]
     if (denom == '2'):
         value = 2
@@ -67,7 +69,8 @@ def getValue(card):
         value = 1
     elif (denom == '-'):
         value = -2
-    elif (denom == 'X'):
+    elif (denom == 'X'): #X represents a face down card
+        #Value of a face down card is based on the average point value of cards remaining in the deck
         value = calculateX(deck, hands, handsShowing)
     return value
 
@@ -84,7 +87,7 @@ deck.append('-j')
 # shuffle
 random.shuffle(deck)
 
-numPlayers = int(input("How many players? "))
+numPlayers = int(input("How many players (including one AI)? "))
 
 hands = []
 handsShowing = []
@@ -114,10 +117,10 @@ while (len(deck) > 0):
             #~~~~~~~~~~~~~~~~~~~~~~~~~~AI~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
             print('AI')
-            print(f'\n{handsShowing[i][0]:2} {handsShowing[i][1]:2}')
+            print(f'\n{handsShowing[i][0]:2} {handsShowing[i][1]:2}') #print hand before AI's turn
             print(f'{handsShowing[i][2]:2} {handsShowing[i][3]:2}')
             print(f'{handsShowing[i][4]:2} {handsShowing[i][5]:2}\n')
-            numX = 0
+            numX = 0 #number of cards still face down
             snagDiscard = False
             snagTop = False
             for card in handsShowing[i]:
@@ -130,28 +133,30 @@ while (len(deck) > 0):
         
             if (numX == 1): #AI is about to go out (one card unflipped)
                 minPlayerVal = 60
-                print("one")
+                #print("one")
                 for p in range(numPlayers - 1):
                     playerPval = calculateHand(handsShowing[p])
                     if (playerPval < minPlayerVal):
                         minPlayerVal = playerPval
                 if ((getValue(discardPile[-1]) + calculateHand(handsShowing[i]) - calculateX(deck, hands, handsShowing) < playerPval - 2 and getValue(discardPile[-1]) < calculateAvg(deck)) or snagDiscard == True): #discard
-                        print("dis")
+                        print("taken from discard:")
                         currentCard = discardPile.pop()
                         takeChoice = 'y'
                 elif (snagTop == True): #deck
-                        print("deck via snag")
+                        print("taken from deck:")
                         currentCard = deck.pop()
                         takeChoice = 'y'
                 elif (getValue(deck[-1]) + calculateHand(handsShowing[i]) - calculateX(deck, hands, handsShowing) < playerPval - 2 and getValue(currentCard) < calculateX(deck, hands, handsShowing)):
-                        print("deck1")
+                        print("taken from deck:")
                         currentCard = deck.pop()
                         takeChoice = 'y'
                 elif (end == True and getValue(discardPile[-1]) < calculateAvg(deck)): #last round
+                        #if top card in discard pile is less than the average value of cards that are face down or in the deck, take discrard
+                        print("taken from discard:")
                         currentCard = discardPile.pop()
                         takeChoice = 'y'
                 else:
-                        print("else")
+                        print("taken from deck:")
                         currentCard = deck.pop()
                         if (end == True and getValue(currentCard) < calculateX(deck, hands, handsShowing)):
                             takeChoice = 'y'
@@ -159,15 +164,16 @@ while (len(deck) > 0):
                             takeChoice = 'n'
         
             elif (getValue(discardPile[-1]) < calculateAvg(deck) or snagDiscard == True): #take from discard
-                print("discard")
+                print("taken from discard:")
                 currentCard = discardPile.pop()
                 takeChoice = 'y'
             else: #take from deck
-                print("deck")
+                print("taken from deck:")
                 currentCard = deck.pop()
                 if (snagTop == True):
                     takeChoice = 'y'
                 elif (getValue(currentCard) < calculateX(deck, hands, handsShowing) + 1.5):
+                    #since you cannot match with face down cards, more cards face up gives an advantage hence the allowance to accept cards that are 1.5 points greater than the average of all cards unflipped or remaining in the deck
                     takeChoice = 'y'
                 else:
                     takeChoice = 'n'
@@ -193,15 +199,15 @@ while (len(deck) > 0):
                 hands[i][putWhere] = currentCard
             elif (takeChoice == 'n'):
                 discardPile.append(currentCard)
-            print(f'{handsShowing[i][0]:2} {handsShowing[i][1]:2}')
+            print(f'{handsShowing[i][0]:2} {handsShowing[i][1]:2}') #print hand after AI's turn
             print(f'{handsShowing[i][2]:2} {handsShowing[i][3]:2}')
             print(f'{handsShowing[i][4]:2} {handsShowing[i][5]:2}\n')
 
-        else:
+        else: #players' turns
             #~~~~~~~~~~~~~~~~~~~~~~~~PLAYERS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
             print('Player', i + 1, '\n')
-            print(f'{handsShowing[i][0]:2} {handsShowing[i][1]:2}')
+            print(f'{handsShowing[i][0]:2} {handsShowing[i][1]:2}') #print hand before player's turn
             print(f'{handsShowing[i][2]:2} {handsShowing[i][3]:2}')
             print(f'{handsShowing[i][4]:2} {handsShowing[i][5]:2}\n')
             if (len(discardPile) != 0):
@@ -231,7 +237,7 @@ while (len(deck) > 0):
             elif (takeChoice == 'n'):
                 discardPile.append(currentCard)
 
-            print(f'\n{handsShowing[i][0]:2} {handsShowing[i][1]:2}')
+            print(f'\n{handsShowing[i][0]:2} {handsShowing[i][1]:2}') #print hand after player's turn
             print(f'{handsShowing[i][2]:2} {handsShowing[i][3]:2}')
             print(f'{handsShowing[i][4]:2} {handsShowing[i][5]:2}')
             print('')
@@ -242,9 +248,9 @@ while (len(deck) > 0):
             for hand in handsShowing:
                 gameFinished = True
                 for card in hand:
-                    if (card == 'X'):
+                    if (card == 'X'): #if every player has at least one un-flipped card (represented by 'X', the game is not complete
                         gameFinished = False
                 if (gameFinished == True):
                     end = True
-                    finishPlayer = k
+                    finishPlayer = k #player k is the first to go out
                 k += 1
